9eac7de76c8b06e726d00c962b929f675634166f
Ibrahim <ibrahimcoderx@gmail.com>
2023-03-13 20:37:57 +0600

    Publish

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,34 @@
-import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+// Common Css
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/common.css";
+import "../styles/order_section.css";
+import "../styles/common_component.css";
+
+// main_page_css
+
+
+// landing Css
+// import "../styles/landing_one/landing_one.css";
+
+import "../styles/landing-16/landing_sixteen.css";
+
+// Theme Css
+
+// import '../styles/theme_two/theme_two_media.css';
+
+
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+          <Component {...pageProps} />
+      {/* </SSRProvider> */}
+    </>
+  );
 }
+
+export default MyApp;
